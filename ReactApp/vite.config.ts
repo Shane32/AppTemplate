9958b8325c91f4b09606f030c4e11691d1e438c6
Shane Krueger <shane@acdmail.com>
2025-12-15 00:08:34 -0500

    Add vite-plugin-compression2 for improved asset compression

--- ReactApp/vite.config.ts
+++ ReactApp/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
+import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [
+    react(),
+    visualizer(),
+    compression({
+      algorithms: ["gzip", "brotliCompress"],
+    }),
+  ],
   build: {
     assetsDir: "static",
   },
